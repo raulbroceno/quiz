a9f35e99d0ce7d69033566bd54296d28a8656b14
Raúl Broceño <raul.broceno@gmail.com>
2015-07-13 17:22:35 +0200

    Aplicado el modelo MVCR

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,11 @@
+exports.question = function(req,res){
+	res.render('quizes/question',{pregunta: 'Capital de Italia'});
+};
+
+exports.answer = function(req,res){
+	if (req.query.respuesta==='Roma'){
+		res.render('quizes/answer',{respuesta: '¡Ole! Acertaste'});
+	}else{
+		res.render('quizes/answer',{respuesta: '¡Noo! Prueba otra vez'});
+	}
+};
