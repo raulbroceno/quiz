2f476485f9fc615d40a915c89682a27b61fedde7
Raúl Broceño <raul.broceno@gmail.com>
2015-08-01 18:39:47 +0200

    Funcionando autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,39 @@
 var models = require('../models/models.js');
 
-exports.question = function(req,res){
-	models.Quiz.findAll().then(function(quiz) {
-		res.render('quizes/question',{pregunta: quiz[0].pregunta});
+//Autoload
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			} else {
+				next(new Error('No existe el identificador QuizId=' + quizId));
+			}
+		}
+	).catch(function(error) {next(error);});
+};
+
+exports.index = function (req, res){
+	models.Quiz.findAll().then(
+		function(quizes) {
+			res.render('quizes/index', { quizes: quizes});
+		}).catch(function(error) { next(error);})
+	};
+
+exports.show = function(req,res){
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+		res.render('quizes/show',{quiz: req.quiz});
 	})
 	//res.render('quizes/question',{pregunta: 'Capital de Italia'});
 };
 
 exports.answer = function(req,res){
-	models.Quiz.findAll().then(function(quiz) {
-		if (req.query.respuesta=== quiz[0].respuesta){
-			res.render('quizes/answer', {respuesta: '¡Ole y ole! Acertaste'});
+	models.Quiz.find(req.params.quizId).then(function(quiz) {
+		if (req.query.respuesta=== req.quiz.respuesta){
+			resultado = '¡Ole y ole! Acertaste';
 		}else{
-			res.render('quizes/answer', {respuesta: '¡Noo! Prueba otra vez'});
+			res.render('quizes/answer', {quiz : req.quiz , respuesta: resultado});
 		}
 	})
 };
--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,15 @@ router.get('/author', function(req, res) {
   res.render('author', { nombre: 'Raúl Broceño' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//Hace el Autoload, localizando el parámetro quizId
+router.param('quizId', quizController.load);
+
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+
+//Definicion de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 module.exports = router;
